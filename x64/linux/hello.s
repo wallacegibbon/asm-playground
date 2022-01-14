a37bf1fb2c85e4efb58cfa7617ce8144d1140bd7
wallace <wallacegibbon@outlook.com>
2022-01-15 00:48:35 +0800

    .

---
+++ x64/linux/hello.s
@@ -0,0 +1,26 @@
+	// this program is not fully functioning yet.
+	// the `sys_write` is not working, but `sys_exit` works.
+	.balign	8
+	.data
+_info:
+	.string "hello, world!\n"
+
+	.balign	8
+	.text
+	.globl _start
+_start:
+	// syscall id is stored in rax
+	mov	$1, %rax
+	// arguments are stored in: rdi, rsi, rdx, r10, r8, r9
+	// sys_write
+	mov	$1, %rdi
+	mov	_info, %rsi
+	mov	$14, %rdx
+	syscall
+
+	// sys_exit
+	mov	$60, %rax
+	mov	$2, %rdi
+	syscall
+
+
